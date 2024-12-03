16061d95ba63f5a42c31b4213dfe6aa3ea59d39b
danielascherer27 <danielascherer27@gmail.com>
2024-12-03 17:26:52 -0300

    Envio final

--- BancoDeDados.java
+++ BancoDeDados.java
@@ -1,32 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class BancoDeDados {
-    private List<Produto> produtos = new ArrayList<>();
-
-    public void salvarProduto(Produto produto) {
-        produtos.add(produto);
-    }
-
-    public boolean removerProduto(String nome) {
-        return produtos.removeIf(produto -> produto.getNome().equals(nome));
-    }
-
-    public boolean atualizarProduto(String nome, Produto produtoAtualizado) {
-        for (int i = 0; i < produtos.size(); i++) {
-            if (produtos.get(i).getNome().equals(nome)) {
-                produtos.set(i, produtoAtualizado);
-                return true;
-            }
+    import java.util.ArrayList;
+    import java.util.List;
+    
+    public class BancoDeDados {
+        private List<Produto> produtos = new ArrayList<>();
+    
+        public void salvarProduto(Produto produto) {
+            produtos.add(produto);
         }
-        return false;
+    
+        public boolean removerProduto(String nome) {
+            return produtos.removeIf(produto -> produto.getNome().equals(nome));
+        }
+    
+        public boolean atualizarProduto(String nome, Produto produtoAtualizado) {
+            for (int i = 0; i < produtos.size(); i++) {
+                if (produtos.get(i).getNome().equals(nome)) {
+                    produtos.set(i, produtoAtualizado);
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        public List<Produto> recuperarProdutos() {
+            return produtos;
+        }
+    
+        public List<Produto> visualizarProdutos() {
+            return produtos;
+        }
     }
-
-    public List<Produto> recuperarProdutos() {
-        return produtos;
-    }
-
-    public List<Produto> visualizarProdutos() {
-        return produtos;
-    }
-}
